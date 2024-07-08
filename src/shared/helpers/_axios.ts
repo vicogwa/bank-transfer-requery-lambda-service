b4a3c827080ba4e5b7836b5ffe27682593d03f87
Dan George <dangeorge35@gmail.com>
2024-07-08 09:59:09 +0100

    Update Template

--- src/shared/helpers/_axios.ts
+++ src/shared/helpers/_axios.ts
@@ -3,9 +3,10 @@ import { createHmac } from 'crypto';
 import { IRequestBody } from '../interfaces';
 
 export const postRequest = async (transaction_ref: string) => {
-    const transactionRequeryUrl = process.env.TransferRequeryEndpoint || '';
-    const request_body: IRequestBody = { transaction_ref: transaction_ref };
-    const signature = await getSignature(request_body);
+    // const transactionRequeryUrl = process.env.TransferRequeryEndpoint || '';
+    const baseUrl = 'https://staging.payment.irechargetech.com';
+    // const transaction_ref: IRequestBody = { transaction_ref: transaction_ref };
+    const signature = await getSignature(transaction_ref);
 
     const headers = {
         'Content-Type': 'application/json',
@@ -13,11 +14,11 @@ export const postRequest = async (transaction_ref: string) => {
         Accept: 'application/json',
     };
 
-    const response = await axios.put(transactionRequeryUrl, request_body, { headers });
+    const response = await axios.put(`${baseUrl}/api/EventHandlers/transfer/${transaction_ref}/requery`, { headers });
     return response.data;
 };
 
-const getSignature = async (request_body: IRequestBody) => {
+const getSignature = async (request_body: string) => {
     const hmac = createHmac('sha256', process.env.WalletHmacKey || '');
     hmac.update(JSON.stringify(request_body), 'utf8');
     const signature = hmac.digest().toString('hex');
